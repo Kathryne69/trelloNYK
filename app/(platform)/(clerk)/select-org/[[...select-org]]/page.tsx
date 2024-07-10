52a0612338b49d23c6baee2a856483d7d769a625
jediii17 <jedidialemuel17@gmail.com>
2024-07-10 14:47:12 +0800

    https://github.com/Kathryne69/NYKtrello.git

--- app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -1,12 +1,11 @@
 import { OrganizationList } from "@clerk/nextjs";
 
 export default function CreateOrganizationPage() {
-    return (
-        <OrganizationList 
-        hidePersonal
-        afterSelectOrganizationUrl="/organization/:id"
-        afterCreateOrganizationUrl="/organization/:id"
-        />
-        
-    );
-};
+  return (
+    <OrganizationList 
+      hidePersonal
+      afterSelectOrganizationUrl="/organization/:id"
+      afterCreateOrganizationUrl="/organization/:id"
+    />
+  );
+}
